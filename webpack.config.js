337e3fd8016ed59e065b09a21338be2ae113a586
Chris Sinco <csinco@google.com>
2021-12-21 11:21:21 -1000

    Refactor into src directory

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = (env, argv) => ({
   devtool: argv.mode === 'production' ? false : 'inline-source-map',
 
   entry: {
-    code: './code.ts', // The entry point for your plugin code
+    code: './src/code.ts', // The entry point for your plugin code
   },
 
   module: {
@@ -29,7 +29,7 @@ module.exports = (env, argv) => ({
   // Tells Webpack to generate "ui.html" and to inline "ui.ts" into it
   plugins: [
     new HtmlWebpackPlugin({
-      template: './ui.html',
+      template: './src/ui.html',
       filename: 'ui.html',
     }),
   ],
